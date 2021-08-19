a7c5461a27bf70c44b2a0c31cc604b6febed819a
kim-soohyeon <kimsh1691@gmail.com>
2021-08-19 15:48:56 +0900

    add() , minus() 함수 작성

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ class App extends React.Component{
   state={
     count: 0
   };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
   render(){
     //this.state: 현재 클래스에 선언한 state를 의미함.
     return <div>
       <h1>The number is: {this.state.count}</h1>
-      <button>Add</button>
-      <button>Minus</button>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>;
   }
+
 }
 
 export default App;

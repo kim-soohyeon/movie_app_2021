7de220e303fbfaea3f5e6ff9854d888836c7c04c
kim-soohyeon <kimsh1691@gmail.com>
2021-08-19 15:41:09 +0900

    state에 count값 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 class App extends React.Component{
-
+  state={
+    count: 0
+  };
+  render(){
+    //this.state: 현재 클래스에 선언한 state를 의미함.
+    return <h1>The number is: {this.state.count}</h1>;
+  }
 }
 
 export default App;

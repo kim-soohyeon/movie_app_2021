fca1c87d4c0e4ac6fd2c735fef1eaa9f14a10137
kim-soohyeon <kimsh1691@gmail.com>
2021-08-22 17:00:20 +0900

    앱 컴포넌트 비우기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import React from "react";
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
-  state={
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count -1 }));
-  };
-  componentDidMount(){
-    console.log('component renderd')
-  }
-  componentDidUpdate(){
-   console.log("I just updated") 
-  }
-  componentWillUnmount(){
-    console.log("Goodbye, cruel World")
-  }
   render(){
-    console.log("I'm rendering");
-    //this.state: 현재 클래스에 선언한 state를 의미함.
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>;
+    return <div />;
   }
-
 }
 
 export default App;

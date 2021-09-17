5a082428f6490e2bb8262d2209f50b034f0cc31a
kim-soohyeon <kimsh1691@gmail.com>
2021-09-17 14:26:38 +0900

    영화 앱 만들기 워밍업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    // 6초 뒤, false로 변경
+    setTimeout(() => {
+      this.setState({isLoading: false})
+    }, 6000);
+  }
   render(){
-    return <div />;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
